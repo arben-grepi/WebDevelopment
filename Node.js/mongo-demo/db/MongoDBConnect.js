b7060ee2a90480249bea3e56f98270dfc4c73f25
arben-grepi <arben.grepi@gmail.com>
2024-07-22 00:52:24 +0300

    added a debugger to file

--- Node.js/mongo-demo/db/MongoDBConnect.js
+++ Node.js/mongo-demo/db/MongoDBConnect.js
@@ -1,7 +1,9 @@
+const dbDebugger = require("debug")("app:db");
+
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/calculator")
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => dbDebugger("Connected to MongoDB"))
   .catch((err) => console.log("Could not connect to MongoDB: ", err));
 
 module.exports = mongoose;
