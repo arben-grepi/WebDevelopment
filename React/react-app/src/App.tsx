da19009e59942be7812635d6caa7414c38e1c765
arben-grepi <arben.grepi@gmail.com>
2024-09-08 14:19:35 +0300

    -

--- React/react-app/src/App.tsx
+++ React/react-app/src/App.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+import { useState } from "react";
+
 function App() {
+  const [bug, setBug] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug2", fixed: false },
+  ]);
+
   return <div></div>;
 }
 export default App;
