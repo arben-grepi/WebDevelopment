0c934804c2bfddbb665b73f62e6f1ee366ffef34
arben-grepi <arben.grepi@gmail.com>
2024-09-17 23:55:52 +0300

    added Icons

--- React/game-hub/src/hooks/usegames.ts
+++ React/game-hub/src/hooks/usegames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
-  background_image: string; // Correct spelling with "g"
+  background_image: string;
+  parent_platforms: { platform: Platform }[]; //design flaw, this doesent make any sense
 }
 
 interface FetchGamesResponse {
